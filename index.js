13ec954ca6f7c236c00b4a2030f34e222b56bdfd
Vipul-28 <vipuldongre348@gmail.com>
2025-02-26 22:03:27 +0530

    Hosted

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173', // Allow only your frontend origin
+    origin: 'https://frontend-placement-portal.vercel.app/', // Allow only your frontend origin
     credentials: true, // Allow cookies and authentication headers
     methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
     allowedHeaders: 'Content-Type,Authorization', // Allowed headers
